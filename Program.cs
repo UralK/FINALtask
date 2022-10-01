b72f5a4c14fba549e19778ac4850c8af717c444a
Ural <kilmyhametov@mail.ru>
2022-10-01 08:04:51 +0500

    Create method Array

--- Program.cs
+++ Program.cs
@@ -7,4 +7,16 @@ void Print(string [] Array){
     Console.WriteLine();
 }
 
+void methodArray (string [] array){
+     string[] newarr = new string[array.Length]
+     for (int i=0; i<array.Length; i++){      
+        if(array[i].Length<=3){
+        newarr[i]=array[i];;
+        Console.WriteLine(newarr[i]); 
+        }
+     }
+        Console.WriteLine(); 
+}
+
 Print(array);
+methodArray(array);
