f05905d83d9ee9f73d03260c4c20be319610b6e3
Ural <kilmyhametov@mail.ru>
2022-10-01 08:07:11 +0500

    fixed bugs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ void Print(string [] Array){
 }
 
 void methodArray (string [] array){
-     string[] newarr = new string[array.Length]
+     string[] newarr = new string[array.Length];
      for (int i=0; i<array.Length; i++){      
         if(array[i].Length<=3){
         newarr[i]=array[i];;
