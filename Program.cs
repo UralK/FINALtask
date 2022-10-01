e0610f5ac376a4d9f2ba977f2558eafa096c7328
Ural <kilmyhametov@mail.ru>
2022-10-01 08:03:27 +0500

    Create new function Print

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿string [] array = {"hello", "2", "world", ":-)"};
+
+void Print(string [] Array){
+    for (int i=0; i<Array.Length; i++){
+        Console.WriteLine(Array[i]);
+    }
+    Console.WriteLine();
+}
+
+Print(array);
